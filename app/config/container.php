fd5a75a1fce961927d06efc2f8799a679918a498
Unknown <contact@simon-micheneau.fr>
2017-07-04 22:12:44 +0200

    Insertion données et modif de l'initiation medoo

--- app/config/container.php
+++ app/config/container.php
@@ -22,30 +22,30 @@ $container['view'] = function ($container) {
 };
 
 // Medoo
-$dataParams = array();
 if(getenv('ENV') == 'local'){
-  $dataParams['DB_TYPE'] = getenv('DBD_TYPE');
-  $dataParams['DB_NAME'] = getenv('DBD_NAME');
-  $dataParams['DB_SERVER'] = getenv('DBD_SERVER');
-  $dataParams['DB_USER'] = getenv('DBD_USER');
-  $dataParams['DB_PWD'] = getenv('DBD_PWD');
+  $container['medoo'] = function () {
+    $medoo = new Medoo\Medoo([
+        'database_type' => getenv('DBD_TYPE'),
+        'database_name' => getenv('DBD_NAME'),
+        'server' => getenv('DBD_SERVER'),
+        'username' => getenv('DBD_USER'),
+        'password' => getenv('DBD_PWD')
+    ]);
+    return $medoo;
+  };
 }elseif(getenv('ENV') == 'prod'){
-  $dataParams['DB_TYPE'] = getenv('DBP_TYPE');
-  $dataParams['DB_NAME'] = getenv('DBP_NAME');
-  $dataParams['DB_SERVER'] = getenv('DBP_SERVER');
-  $dataParams['DB_USER'] = getenv('DBP_USER');
-  $dataParams['DB_PWD'] = getenv('DBP_PWD');
+  $container['medoo'] = function () {
+    $medoo = new Medoo\Medoo([
+      'database_type' => getenv('DBP_TYPE'),
+      'database_name' => getenv('DBP_NAME'),
+      'server' => getenv('DBP_SERVER'),
+      'username' => getenv('DBP_USER'),
+      'password' => getenv('DBP_PWD')
+    ]);
+    return $medoo;
+  };
 }
-$container['medoo'] = function () {
-  $medoo = new Medoo\Medoo([
-      'database_type' => $dataParams['DB_TYPE'],
-      'database_name' => $dataParams['DB_NAME'],
-      'server' => $dataParams['DB_SERVER'],
-      'username' => $dataParams['DB_USER'],
-      'password' => $dataParams['DB_PWD']
-  ]);
-  return $medoo;
-};
+
 
 //Csrf
 $container['csrf'] = function () {
