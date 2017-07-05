6f4733a7dfb3292217b9c73b80913e4d5f66d8e2
Fukotaku <contact@simon-micheneau.fr>
2017-07-05 11:26:03 +0200

    Optimisation de l'init medoo

--- app/config/container.php
+++ app/config/container.php
@@ -22,30 +22,21 @@ $container['view'] = function ($container) {
 };
 
 // Medoo
-if(getenv('ENV') == 'local'){
-  $container['medoo'] = function () {
-    $medoo = new Medoo\Medoo([
-        'database_type' => getenv('DBD_TYPE'),
-        'database_name' => getenv('DBD_NAME'),
-        'server' => getenv('DBD_SERVER'),
-        'username' => getenv('DBD_USER'),
-        'password' => getenv('DBD_PWD')
-    ]);
-    return $medoo;
-  };
-}elseif(getenv('ENV') == 'prod'){
-  $container['medoo'] = function () {
-    $medoo = new Medoo\Medoo([
-      'database_type' => getenv('DBP_TYPE'),
-      'database_name' => getenv('DBP_NAME'),
-      'server' => getenv('DBP_SERVER'),
-      'username' => getenv('DBP_USER'),
-      'password' => getenv('DBP_PWD')
-    ]);
-    return $medoo;
-  };
-}
-
+$container['medoo'] = function () {
+  if (getenv('ENV') == 'local') {
+    $db = "D";
+  } elseif (getenv('ENV') == 'prod') {
+    $db = "P";
+  }
+  $medoo = new Medoo\Medoo([
+      'database_type' => getenv('DB'.$db.'_TYPE'),
+      'database_name' => getenv('DB'.$db.'_NAME'),
+      'server' => getenv('DB'.$db.'_SERVER'),
+      'username' => getenv('DB'.$db.'_USER'),
+      'password' => getenv('DB'.$db.'_PWD')
+  ]);
+  return $medoo;
+};
 
 //Csrf
 $container['csrf'] = function () {
